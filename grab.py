37b1552620c909ef1c4f5c2faea776b0155cf628
MooMinIL <maimon.ya@gmail.com>
2017-08-25 18:04:11 +0300

    Revamped goutham2027's rep for Sanderson's work
    Complete overhaul of goutham2027's source code to parse artwork from Brandon Sanderson's website's artwork section.
    Changed the lookup string and added the ability to scroll through all the pages for a specific book + command line prompt for indication of progress  (it can take a moderately long time to download all the high res images)

--- grab.py
+++ grab.py
@@ -18,25 +18,46 @@ def process_url(raw_url):
      raw_url=raw_url[:-1]
      raw_url=raw_url.replace(' ','%20')
      return raw_url
- 
-url='' ## give the url here
-parse_object=urlparse(url)
-dirname=basename(parse_object.path)
-if not os.path.exists('images'):
-    os.mkdir("images")
-os.mkdir("images/"+dirname)
-os.chdir("images/"+dirname)
- 
-urlcontent=urllib2.urlopen(url).read()
-imgurls=re.findall('img .*?src="(.*?)"',urlcontent)
-for imgurl in imgurls:
- try:
-     imgurl=process_url(imgurl)
-     imgdata=urllib2.urlopen(imgurl).read()
-     filname=basename(urlsplit(imgurl)[2])
-     output=open(filname,'wb')
-     output.write(imgdata)
-     output.close()
-     os.remove(filename)
- except:
-     pass
+
+    
+root_url='https://brandonsanderson.com/books/mistborn/the-final-empire/the-final-empire-art-gallery/nggallery/page/' ## give the url here, should end with '/pages/'
+pg_idx = 1
+print("Parsing all images from sanderson's page: " + root_url)
+EOP = False ## end of pages marker
+while not EOP:
+    url = root_url+format(pg_idx,'1d')
+    print('Trying page #' + format(pg_idx,'1d') + '...')
+    pg_idx = pg_idx+1
+    
+    parse_object = urlparse(url)
+    dirname = basename(parse_object.path)
+    if not os.path.exists('/Users/yairmaimon/Downloads/images/mist'):
+        os.mkdir("/Users/yairmaimon/Downloads/images/mist")
+
+    ## os.mkdir("/Users/yairmaimon/Downloads/images/"+dirname)
+    os.chdir("/Users/yairmaimon/Downloads/images/mist/")
+
+
+    urlcontent=urllib2.urlopen(url).read()
+    print('Finding all the images in page...')
+    imgurls=re.findall('data-src="(.*?)"',urlcontent)
+    idx  = 0
+    EOP = True
+    for imgurl in imgurls:
+        EOP = False
+        idx = idx+1
+        print('Getting image ' + format(idx,'1d') + ' out of ' + format(len(imgurls),'1d'))
+        try:
+            imgurl=process_url(imgurl)
+            imgdata=urllib2.urlopen(imgurl).read()
+            filname=basename(urlsplit(imgurl)[2])
+            output=open(dirname+'_'+format(idx,'02d')+'_'+filname,'wb')
+            output.write(imgdata)
+            output.close()
+            os.remove(filename)
+        except:
+            pass
+    if EOP:
+        print('Nothing Found!')
+       
+print('Finished!')
